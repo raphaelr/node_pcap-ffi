3a842a84ade2b39b04ddd11d9aad556595aad5e0
Raphael Robatsch <raphaelr@f-m.fm>
2012-03-04 23:10:32 +0100

    Moved the stupid terrific interfaces() function into own project

--- test/platform.js
+++ test/platform.js
@@ -2,39 +2,7 @@ var expect = require('expect.js');
 var platform = require('../').platform;
 
 describe('platform', function() {
-	it('should have a string property "pcap_library"', function() {
-		expect(platform.pcap_library).to.be.a('string');
-	});
-	
-	it('should have a function called "interfaces"', function() {
-		expect(platform.interfaces).to.be.a('function');
-	});
-	
-	describe('interfaces', function() {
-		var interfaces;
-		
-		beforeEach(function() {
-			interfaces = platform.interfaces();
-		});
-		
-		it('should return an object', function() {
-			expect(interfaces).to.be.an('object');
-		});
-		
-		it('should contain at least one interface', function() {
-			expect(interfaces).to.not.be.empty();
-		});
-		
-		it('should have a string property "name"', function() {
-			expect(interfaces[0].name).to.be.a('string');
-		});
-		
-		it('should have a string property "description"', function() {
-			expect(interfaces[0].description).to.be.a('string');
-		});
-		
-		it('should have a property "physicalAddress"', function() {
-			expect(interfaces[0]).to.have.property('physicalAddress');
-		});
+	it('should have a string property "PCAP_LIBRARY"', function() {
+		expect(platform.PCAP_LIBRARY).to.be.a('string');
 	});
 });
