be2fa1f4e58aa00ddd83829759849963a08cdb83
Raphael Robatsch <raphaelr@f-m.fm>
2012-03-08 09:58:08 +0100

    Added pcap.openDead()

--- test/platform.js
+++ test/platform.js
@@ -1,8 +1,12 @@
 var expect = require('expect.js');
-var platform = require('../').platform;
+var pcap = require('../');
 
 describe('platform', function() {
 	it('should have a string property "PCAP_LIBRARY"', function() {
-		expect(platform.PCAP_LIBRARY).to.be.a('string');
+		expect(pcap.platform.PCAP_LIBRARY).to.be.a('string');
+	});
+	
+	it('should have a function "systemNameToPcapName"', function() {
+		expect(pcap.systemNameToPcapName).to.be.a('function');
 	});
 });
